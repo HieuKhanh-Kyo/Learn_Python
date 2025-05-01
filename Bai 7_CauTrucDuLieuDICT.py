8f0499a97bc055d62e30fce3bb4a5492347fbce0
Ca Hanh <hieukhanh2k3@gmail.com>
2025-05-01 23:33:59 +0700

    commit

--- Bai 7_CauTrucDuLieuDICT.py
+++ Bai 7_CauTrucDuLieuDICT.py
@@ -1,3 +1,5 @@
 # Kieu tu dien dict:  <key> : <value>
 
 thong_tin_sv = {"Ma SV" : 2021606652, "Ho va Ten" : "Luong Hieu Khanh", "Que quan" : "Bac Giang", "Chuyen nganh" : "QTKD"}
+
+print(thong_tin_sv)

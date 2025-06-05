643681afeb33301a4cbed9ce6f42ad19d83d1693
Ca Hanh <hieukhanh2k3@gmail.com>
2025-06-05 08:14:32 +0700

    commit

--- Bai 8.2_CTQuanLyHocSinh.py
+++ Bai 8.2_CTQuanLyHocSinh.py
@@ -10,30 +10,83 @@
 # 4. In thong tin cua cac HS ra man hinh
 
 
-def nhap_dshs():
+def nhap_tths():
     # Nhap MaHS
     while True:
-        MaHS = input("Nhap ma HS:")
+        MaHS = input("Nhap ma HS: ")
         if len(MaHS.split()) > 0:
             break
 
     # Nhap TenHS
     while True:
-        TenHS = input("Nhap ten HS:")
+        TenHS = input("Nhap ten HS: ")
         if len(TenHS.split()) > 0:
             break
 
     # Nhap Diem Toan
     while True:
-        Diem_Toan = float(("Nhap diem Toan:"))
+        Diem_Toan = float(input("Nhap diem Toan: "))
         if 0 <= Diem_Toan <= 10:
             break
 
     # Nhap Diem Van
     while True:
-        Diem_Van = float(("Nhap diem Van:"))
+        Diem_Van = float(input("Nhap diem Van: "))
         if 0 <= Diem_Van <= 10:
             break
 
-    return {MaHS, TenHS, Diem_Toan, Diem_Van}
+    return {"MaHS" : MaHS,"TenHS" : TenHS, "Diem Toan" : Diem_Toan, "Diem Van" : Diem_Van}
+
+
+def Nhap_DSHS():
+    dsHS = []
+    while True:
+        n = int(input("Nhap so luong HS: "))
+        if n > 0:
+            break
+
+    for i in range(1, n+1):
+        print("Nhap vao thong tin cua HS ", i)
+        dsHS.append(nhap_tths())
+
+    return dsHS
+
+
+def Diem_tb(Diem_Toan, Diem_Van):
+    return (Diem_Toan + Diem_Van)/2
+
+
+def Xep_Loai_HS(Diem_tb):
+    xl = ""
+    if 9 <= Diem_tb <= 10:
+        xl = "Xuat sac"
+    elif 8 <= Diem_tb < 9:
+        xl = "Gioi"
+    elif 7 <= Diem_tb < 8:
+        xl = "Kha"
+    elif 5 <= Diem_tb < 7:
+        xl = "Trung binh"
+    else:
+        xl = "Yeu"
+    return xl
+
+
+def in_TTHS(dsHS):
+    for i in range(len(dsHS)):
+        print("Thong tin cua HS ",i + 1)
+        print("Ma HS la ", dsHS[i]["MaHS"])
+        print("Ten HS la ", dsHS[i]["TenHS"])
+        print("Diem Toan la", dsHS[i]["Diem Toan"])
+        print("Diem Van la ", dsHS[i]["Diem Van"])
+        dtb = Diem_tb(dsHS[i]["Diem Toan"], dsHS[i]["Diem Van"])
+        print("HS xep loai: ", Xep_Loai_HS(dtb))
+
+
+def main():
+    dsHS = Nhap_DSHS()
+    in_TTHS(dsHS)
+
+
+main()
+
 
